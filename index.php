2f20bdb14e990dcd4a91e29c6d0ca20bff5495a9
Dev-GustavoRibeiro <contatogustavoribeirohm@gmail.com>
2024-11-25 15:47:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eita Mainha - Sobre Nós</title>
+    <title>Êta Mainha - Sobre Nós</title>
     <link rel="icon" href="icons/icon-logo.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/boxicons@2.1.1/css/boxicons.min.css" rel="stylesheet">
