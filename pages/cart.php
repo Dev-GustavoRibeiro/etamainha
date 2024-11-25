e0f0bc5754ed942a265a8e4cc3c4bb7a989105cd
Dev-GustavoRibeiro <contatogustavoribeirohm@gmail.com>
2024-11-25 11:18:36 -0300

    Update cart.php

--- pages/cart.php
+++ pages/cart.php
@@ -49,6 +49,7 @@ $carrinho = $_SESSION['carrinho'] ?? [];
     <title>Carrinho de Compras</title>
     <link rel="icon" href="../icons/icon-logo.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Parkinsans:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/cart.css?v=<?= time(); ?>">
 </head>
 <body>
